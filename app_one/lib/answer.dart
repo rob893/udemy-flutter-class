b3b0070eacfaa0d6a8540087db788e7c2bc99fe9
Robert Herber <rwherber@gmail.com>
2021-12-24 16:52:36 -0800

    chore: work on class

---
+++ app_one/lib/answer.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final VoidCallback selectHandler;
+  final String answerText;
+
+  const Answer(this.selectHandler, this.answerText, {Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.blue),
+        ),
+        child: Text(answerText),
+        onPressed: selectHandler,
+      ),
+    );
+  }
+}
--- app_one/lib/main.dart
+++ app_one/lib/main.dart
@@ -1,16 +1,57 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+import './answer.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  int _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex++;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Text('FART'),
+    var questions = [
+      {
+        'questionText': 'What is your favorite color?',
+        'answers': ['Red', 'Green', 'Blue', 'Yellow']
+      },
+      {
+        'questionText': 'What is your favorite animal?',
+        'answers': ['Cat', 'Dog', 'Frog', 'Bug']
+      },
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('FART'),
+        ),
+        body: Column(
+          children: [
+            Question(questions[_questionIndex]['questionText'] as String),
+            ...(questions[_questionIndex]['answers'] as List<String>)
+                .map((answer) => Answer(_answerQuestion, answer))
+                .toList(),
+          ],
+        ),
+      ),
     );
   }
 }
